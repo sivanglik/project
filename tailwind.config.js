9dd4296b6a58629bf12486adff1f31cc01af079b
sivanglik <sivanglik36@gmail.com>
2022-09-19 22:51:33 +0300

    deploy my react app to github pages

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    screens: {
+      'sm': '480px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '547px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '768px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1024px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1680px',
+      // => @media (min-width: 1536px) { ... }
+    }
+  },
+  plugins: [],
+}
